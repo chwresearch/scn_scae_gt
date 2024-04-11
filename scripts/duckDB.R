bb14b059d00b15c8521fa56f61f074ba2ff4f528
Renato <reno@renatovargas.com>
2024-04-10 19:38:23 -0600

    Successfully imported data into DuckDB

--- scripts/duckDB.R
+++ scripts/duckDB.R
@@ -1,8 +1,12 @@
 library("duckdb")
-con <- dbConnect(duckdb(), dbdir = "salidas/scn_gt.duckdb", read_only = FALSE)
-#dbExecute(con, "INSTALL sqlite;")
-#dbExecute(con, "LOAD sqlite;")
-dbGetQuery(con, "ATTACH 'salidas/scn.db' AS test (TYPE sqlite);")
-dbGetQuery(con, "SHOW TABLES;")
 
+if (file.exists("scn_gt.duckdb")) {
+  file.remove("scn_gt.duckdb")
+}
+
+con <- dbConnect(duckdb(), dbdir = "salidas/scn_gt.duckdb", read_only = FALSE)
+dbExecute(con, "INSTALL sqlite;")
+dbExecute(con, "LOAD sqlite;")
+dbExecute(con, "CREATE TABLE scn AS (SELECT * FROM sqlite_scan('salidas/scn.db', 'scn'));")
+dbGetQuery(con, "SHOW TABLES;")
 dbDisconnect(con)
